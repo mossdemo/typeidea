ed8a5ae9981f9a26ba631ef23af44853bc59894f
金强 <jinqiang@xiaohe.com>
2019-06-11 22:40:23 +0800

    修改config app的admin文件

--- typeidea/comment/admin.py
+++ typeidea/comment/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Comment
 from typeidea.custom_site import custom_site
-from typeidea.base_admin import BaseOwnerAdmin
 # Register your models here.
 
 @admin.register(Comment, site=custom_site)
--- typeidea/config/admin.py
+++ typeidea/config/admin.py
@@ -8,8 +8,18 @@ from typeidea.base_admin import BaseOwnerAdmin
 @admin.register(Link, site=custom_site)
 class LinkAdmin(BaseOwnerAdmin):
     list_display = ('title', 'href', 'status', 'weight')
+    fields = ('title', 'href', 'status', 'weight')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(LinkAdmin, self).save_model(request, obj, form, change)
+
 
 @admin.register(SideBar, site=custom_site)
 class SideBarAdmin(BaseOwnerAdmin):
     list_display = ('title', 'display_type', 'content', 'create_time')
     fields = ('title', 'display_type', 'content')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(SideBarAdmin, self).save_model(request, obj, form, change)
